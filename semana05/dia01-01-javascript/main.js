aeee881e162a41cd5a3b9dc536aac3befcb05fcc
Ricardo Guerrero <rguerrerofm@gmail.com>
2026-07-31 21:03:28 -0500

    javascript

---
+++ semana05/dia01-01-javascript/main.js
@@ -0,0 +1,13 @@
+console.log(typeof 10);
+console.log(typeof "hola");
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof nulll);
+
+const a = 10
+const b = 3
+console.log(a/b)
+
+const Base = 10
+const Altura =5
+console.log (Base*Altura/2)
