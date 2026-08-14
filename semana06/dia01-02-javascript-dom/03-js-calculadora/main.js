8122bdee312d0504d4cfc37aabfa13a34b295ff6
Ricardo Guerrero <rguerrerofm@gmail.com>
2026-08-14 00:15:02 -0500

    nuevo js

---
+++ semana06/dia01-02-javascript-dom/03-js-calculadora/main.js
@@ -0,0 +1,36 @@
+let numeroActual = '0'
+let operador = ''
+let operando = ''
+
+// Consultar un elemento a la vez
+const inputDisplay = document.querySelector('#inputDisplay')
+// console.log({inputDisplay})
+// Consultar multiples elementos a la vez
+const buttons = document.querySelectorAll('.button')
+console.log(buttons)
+
+// EVENTOS
+
+buttons.forEach(function(button) {
+  button.addEventListener('click', function(event) {
+    // console.log('Hice click!', event.target)
+
+    const buttonText = event.target.textContent
+
+    if ('*-+'.includes(buttonText)) {
+      // 02. Accionamos el operador
+      operador = buttonText
+      operando = Number(numeroActual)
+      numeroActual = '0'
+    } else if (buttonText === '=') {
+      // TODO: Calcular las operaciones en base al número actual y el operando
+    } else if (buttonText === 'C') {
+      // TODO: Limpiar el operando, operador y el input
+    } else {
+      // 01. Se presionó algún número
+      numeroActual = Number(numeroActual + buttonText)
+    }
+
+    inputDisplay.value = numeroActual
+  })
+})
